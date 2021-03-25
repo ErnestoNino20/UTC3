b9bebd850c0ca2d3b55d14c511841bedfccccead
Ernesto <narutoguero@gmail.com>
2021-03-24 20:49:36 -0600

    Tengo sueño

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Web del nino
+          app de nino
         </a>
       </header>
     </div>
